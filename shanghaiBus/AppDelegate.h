55fa892f456c42affbefbc1f8dc9b783f04261db
liujianzhong <liujianzhong@liujianzhongdeMacBook-Pro.local>
2015-03-21 11:21:24 +0800

    Initial Commit

---
+++ shanghaiBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shanghaiBus
+//
+//  Created by liujianzhong on 15/3/21.
+//  Copyright (c) 2015年 liujianzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
